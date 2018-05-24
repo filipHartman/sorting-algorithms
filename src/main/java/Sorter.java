5ead815ba47b2585c378125e128db97518152c85
filipHartman <filip.hartman23@gmail.com>
2018-05-24 20:00:50 +0200

    Add method that sort array using bubble sort algorithm

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -1,2 +1,14 @@
 public class Sorter {
+    public int[] bubbleSort(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array.length - i-1; j++) {
+                if(array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
 }
