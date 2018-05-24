7dffbb63736610f0eb087abc74792e4cc0cfc064
filipHartman <filip.hartman23@gmail.com>
2018-05-24 21:34:36 +0200

    Add test for insertion sort method

--- src/test/java/SorterTest.java
+++ src/test/java/SorterTest.java
@@ -5,12 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SorterTest {
-    private Sorter sorter;
+    private Sorter sorter = new Sorter();
 
-    @BeforeEach
-    void setup() {
-        sorter = new Sorter();
-    }
     @Test
     void bubbleSort() {
         int[] array = new int[] {2, 9, 6, 1, 8, 7, 4, 0, 5, 3};
@@ -25,4 +21,10 @@ class SorterTest {
         assertArrayEquals(expectedResult, sorter.recursiveBubbleSort(array));
     }
 
+    @Test
+    void insertSort() {
+        int[] array = new int[] {2, 9, 6, 1, 8, 7, 4, 0, 5, 3};
+        int[] expectedResult = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertArrayEquals(expectedResult, sorter.insertSort(array));
+    }
 }
