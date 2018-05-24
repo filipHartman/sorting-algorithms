38c60e60679490d456f145ef06f7916207ec570b
filipHartman <filip.hartman23@gmail.com>
2018-05-24 21:34:07 +0200

    Add implementation of insert sort method

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -1,4 +1,6 @@
 public class Sorter {
+
+    // Method that sort integers using bubble sort implemented using double 'for' loops
     public int[] bubbleSort(int[] array) {
         for(int i = 0; i < array.length; i++) {
             for(int j = 0; j < array.length - i-1; j++) {
@@ -12,6 +14,7 @@ public class Sorter {
         return array;
     }
 
+    // Method that sort integers using bubble sort implemented using recursive strategy
     public int[] recursiveBubbleSort(int[] array) {
         return bubbleSort(array, array.length);
 
@@ -30,5 +33,19 @@ public class Sorter {
             }
         }
         return bubbleSort(array, length-1);
+    }
+
+    public int[] insertSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i-1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+        return array;
     }
 }
